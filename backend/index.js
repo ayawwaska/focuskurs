a2dd004f024d209675457ae2c133e3df0325fab5
Kateryna  Novak <nvk.kateryna@gmail.com>
2023-12-02 19:04:40 +0000

    “backend”

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express  = require('express');
+const httpServer = express();
+const dialer = require('dialer').Dialer;
+
+const config = {
+    url: 'https://uni-call.fcc-online.pl',
+    login: 'focus15',
+    password: '#l9upoqwedsd'
+   };
+
+
+dialer.configure(config);
+   
+
+
+httpServer.listen(3000, function () {
+ console.log('Example app listening on port 3000!')
+});
+
+
+httpServer.get('/call/:number1/:number2', (req, res) => {
+    const number1 = req.params.number1;
+    const number2 = req.params.number2;
+    dialer.call(number1,number2);
+    res.json({success: true});
+});
